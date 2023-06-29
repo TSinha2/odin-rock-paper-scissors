b83675938fcd6df12a3d90c9fd1ae582b42146e7
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-06-29 13:14:01 +0530

    script.js: Add playRound function to determine winner given computer and user inputs

--- script.js
+++ script.js
@@ -2,3 +2,51 @@ function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"]
     return options[Math.floor(Math.random()*options.length)]
 }
+
+function playRound(playerSelection, computerSelection)
+{
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = getComputerChoice().toLowerCase()
+
+    if (playerSelection == computerSelection)
+    {
+        return 0
+    }
+
+    if (playerSelection == "rock")
+    {
+        if (computerSelection == "scissors")
+        {
+            return 1 // User won 
+        }
+        if (computerSelection == "paper")
+        {
+            return -1 // User lost 
+        }        
+    }
+
+    if (playerSelection == "paper")
+    {
+        if (computerSelection == "scissors")
+        {
+            return -1 // User won 
+        }
+        if (computerSelection == "rock")
+        {
+            return 1 // User lost 
+        }        
+    }
+
+    if (playerSelection == "scissors")
+    {
+        if (computerSelection == "paper")
+        {
+            return 1 // User won 
+        }
+        if (computerSelection == "rock")
+        {
+            return -1 // User lost 
+        }        
+    }    
+
+}
